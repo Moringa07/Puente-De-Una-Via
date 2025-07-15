301048b05bee82f79eaa1bb91fd7b111813d4457
merrychi <merryamchitty@gmail.com>
2025-07-15 10:29:55 -0400

    pagina de home creada

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,13 +1,26 @@
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles/home.css'
 export const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-content-box">
+        <h1 className="home-title">
+          Simulación de Vehículos en un Puente de Una Vía
+        </h1>
+        <p className="home-description">
+          Una simulación de un sistema distribuido donde múltiples vehículos
+          intentan cruzar un puente de un solo carril, gestionado por un proceso de servidor central.
+        </p>
+        <p className="home-authors">
+          Creado por: <strong>Merry-am Blanco</strong> y <strong>Mariana Mora</strong>
+        </p>
+        <Link to="/simulacion" className="start-button">
+          Iniciar Simulación
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <>
-            <h1>Bienvenidos a la Simulacion</h1>
-            <button>Comenzar aqui</button>
-        </>
-    )
-
-}
-
+export default Home;
